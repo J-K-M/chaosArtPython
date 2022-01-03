802887f0c0ba7719d8cfa8b04744e75920bec5f2
J-K-M <90958043+J-K-M@users.noreply.github.com>
2022-01-03 15:21:29 +0000

    Add files via upload

--- chunkMethod/cg.py
+++ chunkMethod/cg.py
@@ -4,22 +4,23 @@ from PIL import Image,ImageDraw,ImageTk
 
 class ChaosGenerator:
     def __init__(self, parentWindow, width=1280, height=720) -> None:
-
+        self.parentWindow = parentWindow
         self.width = width
         self.height = height
         self.backgroundColour = (10,10,12)
 
-        self.numPoints = 100000
+        self.numPoints = 1000000
         self.scale = width*0.2
         self.pixelSize = 0.5 # actual pixel size is double this value
         self.pixelColourFunc = lambda i, n: ( 
             int(128*(i/n)+128),     # R
             int(128*(i/n)+128),     # G
-            int(220 - 128*(i/n))    # B
+            int(220 - 128*(i/n)),   # B
+            int(50)                 # A
         )
 
         self.baseImage = Image.new("RGB", (self.width, self.height), self.backgroundColour)
-        self.draw = ImageDraw.Draw(self.baseImage)
+        self.draw = ImageDraw.Draw(self.baseImage, mode="RGBA")
         self.tkImage = ImageTk.PhotoImage(self.baseImage)
 
         self.f = ttk.Frame(parentWindow)
@@ -70,55 +71,52 @@ class ChaosGenerator:
 
 
     def updateImagePoints(self, t=0, x=0, y=0, count=0, pixelNum=1000):
-        if self.f.winfo_exists():
+        if count == 0:
+            self.draw.rectangle((0,0,self.width,self.height), fill=self.backgroundColour)
 
+        if count >= self.numPoints:
+            self.running = None
+            print("done")
 
-            if count == 0:
-                self.draw.rectangle((0,0,self.width,self.height), fill=self.backgroundColour)
+        for i in range(pixelNum):
+            if x < self.width and y < self.height:
+                self.draw.rectangle(
+                    (
+                        (x*self.scale + self.width/2) - self.pixelSize,
+                        (y*self.scale + self.height/2) - self.pixelSize,
+                        (x*self.scale + self.width/2) + self.pixelSize,
+                        (y*self.scale + self.height/2) + self.pixelSize,
+                    ),
+                    fill=self.pixelColourFunc(count, self.numPoints)
+                )   
+                count += 1
 
-            if count >= self.numPoints:
-                self.running = None
-                print("done")
+                try:
+                    nx = -x**2 -t**2 + x*t - y*t - x
+                    ny = -x**2 -t**2 + x*t - x - y
 
-            for i in range(pixelNum):
-                if x < self.width and y < self.height:
-                    self.draw.rectangle(
-                        (
-                            (x*self.scale + self.width/2) - 0.5,
-                            (y*self.scale + self.height/2) - 0.5,
-                            (x*self.scale + self.width/2) + 0.5,
-                            (y*self.scale + self.height/2) + 0.5,
-                        ),
-                        fill=self.pixelColourFunc(count, self.numPoints)
-                    )   
-                    count += 1
+                    x, y = nx, ny
+                except OverflowError:
+                    pass
+        
 
-                    try:
-                        nx = -x**2 -t**2 + x*t - y*t - x
-                        ny = -x**2 -t**2 + x*t - x - y
+        bottom = self.height * .8
+        top = self.height * .2
+        left = self.width * .965
+        right = self.width * .975
+        
+        self.draw.rectangle((left, bottom, right, top), fill="white")
+        self.draw.rectangle((left, bottom, right, bottom + (top-bottom) *(count/self.numPoints)), fill="grey")
 
-                        x, y = nx, ny
-                    except OverflowError:
-                        pass
-            
+        self.tkImage = ImageTk.PhotoImage(self.baseImage)
+        self.l.configure(image = self.tkImage)
 
-            bottom = self.height * .8
-            top = self.height * .2
-            left = self.width * .965
-            right = self.width * .975
-            
-            self.draw.rectangle((left, bottom, right, top), fill="white")
-            self.draw.rectangle((left, bottom, right, bottom + (top-bottom) *(count/self.numPoints)), fill="grey")
-
-            self.tkImage = ImageTk.PhotoImage(self.baseImage)
-            self.l.configure(image = self.tkImage)
-
-            if count < self.numPoints:
-                self.running = self.f.after_idle(
-                    lambda: self.updateImagePoints(
-                        t=t,
-                        x=x,
-                        y=y,
-                        count=count
-                    )
+        if count < self.numPoints:
+            self.running = self.parentWindow.after_idle(
+                lambda: self.updateImagePoints(
+                    t=t,
+                    x=x,
+                    y=y,
+                    count=count
                 )
+            )
