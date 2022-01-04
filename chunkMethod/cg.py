d8737c8332b58a97233f2502a4a30792033b028e
J-K-M <90958043+J-K-M@users.noreply.github.com>
2022-01-04 13:47:45 +0000

    Add 'save image' feature
    Modified cg.py and interactive.py to allow for saving images.
    Also added paramater to adjust the resolution of the image shown in tkinter vs the actual image resolution.
    (tkinter preview can be lower res)

--- chunkMethod/cg.py
+++ chunkMethod/cg.py
@@ -1,29 +1,33 @@
 from tkinter import *
 from tkinter import ttk
 from PIL import Image,ImageDraw,ImageTk
+from random import randint
 
 class ChaosGenerator:
-    def __init__(self, parentWindow, width=1280, height=720) -> None:
+    def __init__(self, parentWindow, width=1280, height=720, 
+                scale=0.2, xOffset=0.5, yOffset=0.5, previewScale=2) -> None:
+
         self.parentWindow = parentWindow
         self.width = width
         self.height = height
         self.backgroundColour = (10,10,12)
 
         self.numPoints = 1000000
-        self.scale = width*0.2
-        self.pixelSize = 1
-        self.pixelColourFunc = lambda i, n: ( 
-            int(128*(i/n)+128),     # R
-            int(128*(i/n)+128),     # G
-            int(220 - 128*(i/n)),   # B
-            int(50)                 # A
-        )
-        self.xFunc = lambda x, y, t: -x**2 -t**2 + x*t - y*t - x
-        self.yfunc = lambda x, y, t: -x**2 -t**2 + x*t - x - y
+        self.xScale = width * scale
+        self.yScale = height * scale
 
-        self.baseImage = Image.new("RGB", (self.width, self.height), self.backgroundColour)
+        self.previewScale = previewScale
+
+        self.baseImage = Image.new(
+            "RGB", (self.width, self.height), 
+            self.backgroundColour
+        )
         self.draw = ImageDraw.Draw(self.baseImage, mode="RGBA")
-        self.tkImage = ImageTk.PhotoImage(self.baseImage)
+        self.tkImage = ImageTk.PhotoImage(
+            self.baseImage.resize(
+                (self.width//previewScale, 
+                self.height//previewScale)
+        ))
 
         self.f = ttk.Frame(parentWindow)
         self.l = ttk.Label(self.f, image=self.tkImage)
@@ -31,59 +35,62 @@ class ChaosGenerator:
 
         self.running = None
 
+        self.bottom = self.height * .8
+        self.top = self.height * .2
+        self.left = self.width * .965
+        self.right = self.width * .975
 
-    def generateEquations(self, code):
-        print(code)
+        self.xCenter = width * xOffset
+        self.yCenter = height * yOffset
 
-    def generateCode(self):
-        print(self.xFunc)
-        print(self.yfunc)
-
-
-    def updateImagePoints(self, t=0, x=0, y=0, count=0, pixelNum=1000):
+    def updateImagePoints(self, t=0, x=0, y=0, count=0, pixelNum=20000):
         if count == 0:
-            self.draw.rectangle((0,0,self.width,self.height), fill=self.backgroundColour)
-
-        if count >= self.numPoints:
-            self.running = None
-            print("done")
+            opacity = 60 # Higher opacity on first pass for clearer preview
+            self.draw.rectangle(
+                (0,0,self.width,self.height), 
+                fill=self.backgroundColour)      # Clears points
+        else:
+            opacity = 20
 
         for i in range(pixelNum):
             if x < self.width and y < self.height:
 
-                x1 = int(x*self.scale + self.width/2)  #- self.pixelSize/2
-                y1 = int(y*self.scale + self.height/2) #- self.pixelSize/2
-                x2 = x1 + self.pixelSize
-                y2 = y1 + self.pixelSize
-                self.draw.point(
-                    ( x1, y1),
-                    fill=( 
-                        255,  # R
-                        255,  # G
-                        255,  # B
-                        30    # A
-                    )
-                )   
-                count += 1
-
+                xpos = int(x*self.xScale + self.xCenter)
+                ypos = int(y*self.yScale + self.yCenter)
                 try:
+                    self.draw.point(
+                        ( xpos, ypos),
+                        fill=(
+                            int(128 + 64*count/self.numPoints), 
+                            int(128 + 128*count/self.numPoints), 
+                            int(255 - 128*count/self.numPoints), 
+                            opacity
+                        )
+                    )   
+                    count += 1
+
                     nx = -x**2 -t**2 + x*t - y*t - x
                     ny = -x**2 -t**2 + x*t - x - y
 
                     x, y = nx, ny
-                except OverflowError:
+                except:
                     pass
         
+        self.draw.rectangle(
+            (self.left, self.bottom, self.right, self.top), 
+            fill="white"
+        )
+        self.draw.rectangle(
+            (self.left, self.bottom, self.right, 
+            self.bottom + (self.top-self.bottom) *(count/self.numPoints)), 
+            fill="grey"
+        )
 
-        bottom = self.height * .8
-        top = self.height * .2
-        left = self.width * .965
-        right = self.width * .975
-        
-        self.draw.rectangle((left, bottom, right, top), fill="white")
-        self.draw.rectangle((left, bottom, right, bottom + (top-bottom) *(count/self.numPoints)), fill="grey")
-
-        self.tkImage = ImageTk.PhotoImage(self.baseImage)
+        self.tkImage = ImageTk.PhotoImage(
+            self.baseImage.resize(
+                (self.width//self.previewScale, 
+                self.height//self.previewScale)
+        ))
         self.l.configure(image = self.tkImage)
 
         if count < self.numPoints:
--- chunkMethod/interactive.py
+++ chunkMethod/interactive.py
@@ -1,35 +1,50 @@
 from tkinter import *
+from tkinter import simpledialog
 from cg import ChaosGenerator
-import cProfile
 
+# Initialising and configuring root window
 root = Tk()
 root.title("Chaos Art Generator")
 root.rowconfigure(0, weight=1)
 root.rowconfigure(2, weight=1)
 root.columnconfigure(0, weight=1)
-root.columnconfigure(2, weight=1)
+root.columnconfigure(3, weight=1)
 
-cg = ChaosGenerator(root, width=1500, height=900)
-cg.f.grid(row=1,column=1)
+# Initialising and placing image generator
+cg = ChaosGenerator(root, width=2000, height=1500, scale=0.3, xOffset=0.6)
+cg.f.grid(row=2,column=1, columnspan=5)
 
+# Initialising and placing slider to change t value
 scaleRes = 0.0001
 s = Scale(
     root, 
     from_ = -0.4, 
-    to = 0.1 , 
+    to = 0.1, 
     orient = HORIZONTAL, 
-    resolution=scaleRes, 
-    sliderlength=100
+    resolution=scaleRes,
+    sliderlength=80
 )
-s.grid(row=2, column=1, sticky="EW")
-root.bind("j", lambda e: s.set(s.get()-scaleRes))
-root.bind("k", lambda e: s.set(s.get()+scaleRes))
-root.bind("g", lambda e: s.set(s.get()-scaleRes*10))
-root.bind("h", lambda e: s.set(s.get()+scaleRes*10))
+s.grid(row=3, column=1, columnspan=5, sticky="NSEW")
+# binding keyboard shortcuts for adjusting t value
 root.bind("d", lambda e: s.set(s.get()-scaleRes*100))
 root.bind("f", lambda e: s.set(s.get()+scaleRes*100))
-root.bind("<Escape>", lambda e: root.destroy())
+root.bind("g", lambda e: s.set(s.get()-scaleRes*10))
+root.bind("h", lambda e: s.set(s.get()+scaleRes*10))
+root.bind("j", lambda e: s.set(s.get()-scaleRes))
+root.bind("k", lambda e: s.set(s.get()+scaleRes))
 sVal = s.get()
+
+def saveImage():
+    img = cg.baseImage
+    fileName = simpledialog.askstring("Save Image", "file name: ")
+    fileName = f"./images/{fileName}.png"
+    print(f"file saved to {fileName}.png")
+    print(fileName)
+    img.save(fileName,"PNG")
+
+
+Label(root,text=f"{cg.numPoints:,} points").grid(row=1, column=1)
+Button(root, text="Save Image", command=saveImage).grid(row=1, column=5)
 
 def main():
     root.after(1, checkChange)
@@ -46,5 +61,4 @@ def checkChange():
         cg.updateImagePoints(t=sVal)
 
 if __name__ == "__main__":
-    # cProfile.run("main()")
     main()
